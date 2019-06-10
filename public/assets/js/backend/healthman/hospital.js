2e604c016a84953ea8e1b15c91dd494e2c1066fd
Pinng <443977226@qq.com>
2019-06-11 00:24:19 +0800

    1.0.0

--- public/assets/js/backend/healthman/hospital.js
+++ public/assets/js/backend/healthman/hospital.js
@@ -26,6 +26,32 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefin
                         {checkbox: true},
                         {field: 'id', title: __('Id')},
                         {field: 'flagship', title: __('Flagship'), searchList: {"0":__('Flagship 0'),"1":__('Flagship 1'),"2":__('Flagship 2')}, formatter: Table.api.formatter.normal},
+                        {
+                            field: 'buttons',
+                            width: "120px",
+                            title: __('相关事项'),
+                            table: table,
+                            events: Table.api.events.operate,
+                            buttons: [
+                                {
+                                    name: 'detail',
+                                    text: __('相关事项'),
+                                    title: __('添加量级轨迹'),
+                                    classname: 'btn btn-xs btn-primary btn-dialog',
+                                    icon: 'fa fa-list',
+                                    url: 'healthman/hostrace/add',
+                                    callback: function (data) {
+                                        Layer.alert("接收到回传数据：" + JSON.stringify(data), {title: "回传数据"});
+                                    },
+                                    visible: function (row) {
+                                        //返回true时按钮显示,返回false隐藏
+                                        return true;
+                                    }
+                                }
+                            ],
+                            formatter: Table.api.formatter.buttons
+                        },
+                        {field: 'category', title: __('Category')},
                         {field: 'name', title: __('Name')},
                         {field: 'location', title: __('Location')},
                         {field: 'status', title: __('Status'), searchList: {"0":__('Status 0'),"1":__('Status 1'),"2":__('Status 2')}, formatter: Table.api.formatter.status},
@@ -35,7 +61,6 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefin
                         {field: 'title', title: __('Title')},
                         {field: 'address', title: __('Address')},
                         {field: 'tel', title: __('Tel')},
-                        {field: 'category', title: __('Category')},
                         {field: 'adcode', title: __('Adcode')},
                         {field: 'province', title: __('Province')},
                         {field: 'city', title: __('City')},
